a950f43e7a38c0bee9bb1f0e65e79ed69826b5aa
kulbandita <kulbandita@gmail.com>
2017-07-20 13:15:52 +0700

    removeNonDigit_with_dash() is done

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -1,7 +1,14 @@
 public class FormatPhoneNumber {
 
     public String removeNonDigit(String input) {
-        return null;
+        String digit = "";
+        int length = input.length();
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) != '-') {
+                digit += input.charAt(i);
+            }
+        }
+        return digit;
     }
 
     public String solution(String input) {
