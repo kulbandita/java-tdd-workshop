8f71dba75f7b1df8ec7912559d8ec9e058591e24
kulbandita <kulbandita@gmail.com>
2017-07-20 14:37:57 +0700

    should_return_3_when_number_is_3() is done

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -5,7 +5,7 @@ public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> primeNumber = new ArrayList<Integer>();
 
-        if (number == 2) {
+        if (number == 2 || number == 3) {
             primeNumber.add(number);
         }
 
