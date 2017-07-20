fa819897eeaa7f626fb7a40d59d6b99fcf7a893d
kulbandita <kulbandita@gmail.com>
2017-07-20 15:09:24 +0700

    should_return_3_3_3_when_number_is_27() is done

--- prime_factor/src/test/java/PrimeFactorTest.java
+++ prime_factor/src/test/java/PrimeFactorTest.java
@@ -50,7 +50,7 @@ public class PrimeFactorTest {
         assertEquals(list(3, 3), primeFactor.of(9));
     }
 
-    @Test @Ignore
+    @Test
     public void should_return_3_3_3_when_number_is_27() {
         PrimeFactor primeFactor = new PrimeFactor();
         assertEquals(list(3, 3, 3), primeFactor.of(27));
