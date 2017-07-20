d11baeea6d3d926611d36c30b387fd7ba3d6fa39
kulbandita <kulbandita@gmail.com>
2017-07-20 14:36:18 +0700

    should_return_2_when_number_is_2() is done

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -4,9 +4,11 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> primeNumber = new ArrayList<Integer>();
-        if(number == 1){
-            return primeNumber;
+
+        if (number == 2) {
+            primeNumber.add(number);
         }
-        return null;
+
+        return primeNumber;
     }
 }
