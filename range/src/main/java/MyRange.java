ff9173e538e22a9e4420b67b2a921215388f5fce
kulbandita <kulbandita@gmail.com>
2017-07-20 11:47:19 +0700

    ตรวจสอบว่าเป็น_ก้ามปูปิด() is done

--- range/src/main/java/MyRange.java
+++ range/src/main/java/MyRange.java
@@ -7,11 +7,13 @@ public class MyRange {
         return 0;
     }
 
-    public boolean isก้ามปูปิด(String input) { return false; }
+    public boolean isก้ามปูปิด(String input) {
+        return input.endsWith("]");
+    }
 
     public int getFirstNumber(String input) {
         int firstNumber = Character.getNumericValue(input.charAt(1));
-        if (isก้ามปูเปิด(input)){
+        if (isก้ามปูเปิด(input)) {
             return firstNumber;
         }
         return firstNumber + 1;
