9a9ad52e24c95ef4272d0111cf3adfd792e7d5f0
kulbandita <kulbandita@gmail.com>
2017-07-20 11:51:53 +0700

    หาตัวเลขสุดท้าย2() is done

--- range/src/main/java/MyRange.java
+++ range/src/main/java/MyRange.java
@@ -4,7 +4,11 @@ public class MyRange {
     }
 
     public int getLastNumber(String input) {
-        return Character.getNumericValue(input.charAt(3));
+        int lastNumber = Character.getNumericValue(input.charAt(3));
+        if (isก้ามปูปิด(input)) {
+            return lastNumber;
+        }
+        return lastNumber - 1;
     }
 
     public boolean isก้ามปูปิด(String input) {
