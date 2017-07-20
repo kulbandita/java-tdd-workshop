d51ee52b01ad64bb2580763833d8c1eb98b0453f
Bandita Kulthammanon <bandita.k@kasikornbank.com>
2017-07-20 11:02:50 +0700

    should_say_FizzBuzz_when_number_is_15() is done

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,9 +1,10 @@
 public class FizzBuzz {
     public String say(int input) {
-        if (input % 3 == 0) {
+        if (input % 15 == 0) {
+            return "FizzBuzz";
+        } else if (input % 3 == 0) {
             return "Fizz";
-        }
-        if(input % 5 == 0){
+        } else if (input % 5 == 0) {
             return "Buzz";
         }
         return String.valueOf(input);
