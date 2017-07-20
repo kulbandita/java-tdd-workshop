6b467b55358e7ed8733520202792beebe6a09049
kulbandita <kulbandita@gmail.com>
2017-07-20 15:05:49 +0700

    should_return_2_2_2_when_number_is_8() is done

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -5,13 +5,14 @@ public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> primeNumber = new ArrayList<Integer>();
 
-        if (number == 2 || number == 3) {
-            primeNumber.add(number);
-        } else {
-            if (number % 2 == 0) {
+        if (number % 2 == 0) {
+            while (number >= 4) {
+                number /= 2;
                 primeNumber.add(2);
-                primeNumber.add(number / 2);
             }
+            primeNumber.add(number);
+        } else if (number % 3 == 0) {
+            primeNumber.add(number);
         }
 
         return primeNumber;
