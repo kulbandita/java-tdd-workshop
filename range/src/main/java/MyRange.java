36ee52e4d4449227888df8542b70c87cfb9a48e9
kulbandita <kulbandita@gmail.com>
2017-07-20 11:44:05 +0700

    หาตัวเลขเริ่มต้น3() is done

--- range/src/main/java/MyRange.java
+++ range/src/main/java/MyRange.java
@@ -10,7 +10,11 @@ public class MyRange {
     public boolean isก้ามปูปิด(String input) { return false; }
 
     public int getFirstNumber(String input) {
-        return Character.getNumericValue(input.charAt(1));
+        int firstNumber = Character.getNumericValue(input.charAt(1));
+        if (isก้ามปูเปิด(input)){
+            return firstNumber;
+        }
+        return firstNumber + 1;
     }
 
     public boolean isก้ามปูเปิด(String input) {
