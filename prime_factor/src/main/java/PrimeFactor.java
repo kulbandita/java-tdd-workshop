8f6072c3201023dc25f01be603f706b2e5b075c1
kulbandita <kulbandita@gmail.com>
2017-07-20 14:33:54 +0700

    should_return_empty_when_number_is_1() is done

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 public class PrimeFactor {
     public List<Integer> of(int number) {
+        List<Integer> primeNumber = new ArrayList<Integer>();
+        if(number == 1){
+            return primeNumber;
+        }
         return null;
     }
 }
